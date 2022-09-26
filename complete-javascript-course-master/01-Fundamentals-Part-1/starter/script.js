4603cb7f4441ddbf3d51774c01197d4b40e9f6fa
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-09-26 12:26:30 +0300

    Daily commit

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,17 @@
-let js = 'fun';
+const markWeight = 78;
+const markHeight = 1.69;
 
-if (js === 'fun') {
-  console.log('Hello, World!')
-};
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const markBMI = (markWeight / markHeight ** 2).toFixed(1);
+const johnBMI = (johnWeight / johnHeight ** 2).toFixed(1);
+
+const markHigherBMI = markBMI > johnBMI;
+
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+}
