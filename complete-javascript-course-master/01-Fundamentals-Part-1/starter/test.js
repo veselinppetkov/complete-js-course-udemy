bcdca2a9c7fa29a017e550da3f1406a73053c263
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-09-27 12:15:59 +0300

    Assignments finished

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/test.js
@@ -0,0 +1,12 @@
+function yearsOld(birthYear) {
+    return 2022 - birthYear;
+};
+
+console.log(yearsOld(1997));
+
+const yearsOldExpression = function (birthYear) {
+    return 2023 - birthYear;
+};
+
+console.log(yearsOldExpression(1997));
+
